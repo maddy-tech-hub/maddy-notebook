b03724ee854829453a845343ddbcd61b1225b33b
Madhava Reddy <90255418+Madhava-Reddy@users.noreply.github.com>
2026-07-14 06:32:01 +0530

    Update RouterProvider.tsx

--- src/Practice/ReactConcepts/RouterProvider.tsx
+++ src/Practice/ReactConcepts/RouterProvider.tsx
@@ -1,45 +1,66 @@
-
+import {
+    createBrowserRouter,
+} from "react-router-dom";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  const isAuthenticated = true; 
+import { NavLink } from "react-router-dom";
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+export const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    );
 };
 
-export default ProtectedRoute;
+export const Navbar = () => {
+    return (
+        <nav>
+            <NavLink to="/dashboard">Dashboard</NavLink>{" "}
+            <NavLink to="/profile">Profile</NavLink>{" "}
+            <NavLink to="/settings">Settings</NavLink>{" "}
+            <NavLink to="/">Login</NavLink>
+        </nav>
+    );
+};
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+const ProtectedRoute = () => {
+    const isAuthenticated = true;
 
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+};
 
-const Login = () => { return (<p>Hello Welcome to <b>Login</b> Page.</p>) }
-const Dashboard = () => { return (<p>Hello Welcome to <b>Dashboard</b> Page.</p>) } 
+const Login = () => { return (<Navigate to="/dashboard" replace />) }
+const Dashboard = () => { return (<p>Hello Welcome to <b>Dashboard</b> Page.</p>) }
 const Profile = () => { return (<p>Hello Welcome to <b>Profile</b> Page.</p>) }
 const Settings = () => { return (<p>Hello Welcome to <b>Settings</b> Page.</p>) }
 
 export const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/settings",
-        element: <Settings />,
-      },
-    ],
-  },
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        element: <ProtectedRoute />,
+        children: [
+            {
+                element: <Layout />,
+                children: [
+                    {
+                        path: "/dashboard",
+                        element: <Dashboard />,
+                    },
+                    {
+                        path: "/profile",
+                        element: <Profile />,
+                    },
+                    {
+                        path: "/settings",
+                        element: <Settings />,
+                    },
+                ],
+            },
+        ],
+    },
 ]);
